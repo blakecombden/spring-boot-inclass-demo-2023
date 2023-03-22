1afb2ff47dd9f8b824f28117912eac1df491ad1e
JoshTaylorNL <jktylr@gmail.com>
2023-03-22 09:23:30 -0230

    Initial commit

---
+++ src/test/java/com/example/springbootinclassdemo2023/SpringBootInclassDemo2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinclassdemo2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInclassDemo2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
